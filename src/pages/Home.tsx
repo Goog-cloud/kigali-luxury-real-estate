e07fa7356a64e81b864e6ccad66bd3120ae562f2
unknown <karasiraian3@gmail.comgit config --global user.email karasiraian3@gmail.com>
2025-05-06 19:24:13 +0200

    new changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import Header from "../components/Header/Header";
 import MiddleOne from "../components/Body/MiddleOne";
 // import MiddleTwo from "../components/Body/MiddleTwo";
-import MiddleThree from "../components/Body/MiddleThree";
-import MiddleFour from "../components/Body/MiddleFour";
+// import MiddleThree from "../components/Body/MiddleThree";
+// import MiddleFour from "../components/Body/MiddleFour";
 import MiddleFive from "../components/Body/MiddleFive";
 import MiddleSix from "../components/Body/MiddleSix";
 import HouseCards from "../components/Body/HouseCards";
